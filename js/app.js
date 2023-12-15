5f61876c5d9b59ad0e4b49e4e7f9bf73cb391648
dymovv1 <romandymovv1997@gmail.com>
2023-12-15 19:06:08 +0100

    fixed background , set on pc slider

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const navLiList = document.querySelectorAll('.header-nav__li');
-
-navLiList.forEach(function(navLi) {
-    navLi.addEventListener('click', function(event) {
-        event.preventDefault(); 
-        navLi.classList.add('is-active');
-    });
-});
-
